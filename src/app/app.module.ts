24b47659b06dbcf4ccef25f0fa37d9d6cbdb922c
highbury1999 <henryliaoxuyang@gmail.com>
2019-02-01 00:33:44 +1100

    Clear code and reset project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,32 +6,20 @@ import { HttpModule }    from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 // Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }         from './app.component';
-import { DashboardComponent }   from './dashboard.component';
-import { HeroesComponent }      from './heroes.component';
-import { HeroDetailComponent }  from './hero-detail.component';
-import { HeroService }          from './hero.service';
-import { HeroSearchComponent }  from './hero-search.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    HeroDetailComponent,
-    HeroesComponent,
-    HeroSearchComponent
+    AppComponent
   ],
-  providers: [ HeroService ],
+  providers: [  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
